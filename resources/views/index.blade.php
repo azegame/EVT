c605969bce9674e0b88664b4024234f814e7e573
azegame <a.masashi218@gmail.com>
2023-08-19 20:49:03 +0900

    CSS調整

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -19,12 +19,16 @@
                                 <div class="flex flex-wrap -m-2">
                                     <div class="p-2 w-full">
                                         <div class="relative">
-                                            <label for="entext" class="leading-7 text-sm text-gray-600">テキスト</label>
-                                            <textarea id="entext" name="text" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+                                            <label for="text" class="leading-7 text-sm text-gray-600">テキスト</label>
+                                            <textarea id="text" name="text" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
                                         </div>
                                     </div>
                                     <div class="p-2 w-full">
-                                        <button class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">保存する</button>
+                                        <div class="flex justify-center space-x-4">
+                                            <button id="speak-btn" class="flex text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">再生</button>
+                                            <button class="flex text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">保存する</button>
+                                        </div>
+                                        <div class="mt-7"></div>
                                     </div>
                                 </div>
                             </div>
@@ -34,8 +38,4 @@
             </div>
         </div>
     </div>
-    <select id="voice-names"></select>
-    <br />
-    <input id="rate" type="range" min="0" max="2" step="0.25" value="1"> (speed)
-    <br />
 </x-app-layout>
