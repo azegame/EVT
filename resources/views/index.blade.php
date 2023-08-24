dc87bbd0752d85ade05ce90f5c6fb68e469f7ee2
azegame <a.masashi218@gmail.com>
2023-08-24 23:57:52 +0900

    ダークモードも

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -24,7 +24,7 @@
                                         </div>
                                     </div>
                                     <div class="p-2 w-full text-center">
-                                        <label for="vol" class="col-3 font-weight-bold text-center">
+                                        <label for="vol" class="col-3 font-weight-bold text-center dark:text-gray-200">
                                             速度:
                                             <span id="speedLabel">({{ old('speed', '1.0') }})</span></label>
                                         <input type="range" id="id_speed" name="speed" class="col-7 offset-1" step="0.1" min="0.1" max="3.0" value="1.0" required>
