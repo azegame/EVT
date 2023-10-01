47eef1bf24d45d1ca48e4c27c7e20718df4ff92b
azegame <a.masashi218@gmail.com>
2023-10-02 00:21:15 +0900

    clean js file

--- resources/js/speak.js
+++ resources/js/speak.js
@@ -1,13 +1,13 @@
-const textElement = document.querySelector('#text')
-const speakBtn = document.querySelector('#speak-btn')
-const stopBtn = document.querySelector('#stop-btn')
-const rateInput = document.querySelector('#id_speed')
+const textElement = document.querySelector('#text');
+const speakBtn = document.querySelector('#speak-btn');
+const stopBtn = document.querySelector('#stop-btn');
+const rateInput = document.querySelector('#id_speed');
 const speedLabel = document.getElementById('speedLabel');
 
 // ② 使える声が追加されたときに着火するイベントハンドラ。
 // Chrome は非同期に(一個ずつ)声を読み込むため必要。
 speechSynthesis.onvoiceschanged = (e) => {
-    //appendVoices()
+    //appendVoices();
 }
 
 // input要素の値が変更されたときに速度ラベルを更新するイベントリスナーを追加
@@ -16,48 +16,41 @@ rateInput.addEventListener('input', () => {
 });
 
 speakBtn.addEventListener('click', () => {
-    let btnText = document.querySelector('#speak-btn').textContent;
-    document.querySelector('#stop-btn').innerHTML = '一時停止'
-
+    document.querySelector('#stop-btn').innerHTML = '一時停止';
     const text = textElement.value;
 
     if (text.match(/[^\x01-\x7E\uFF61-\uFF9F]+/)) {
         //全角文字
-        alert('全角文字は入力できません。');
+        alert('全角文字は再生できません。');
     } else {
         //全角文字以外
         console.log("全角文字ではありません");
 
         const splitTextArr = text.split(/[,\.]/);
-        console.log(splitTextArr);
 
         speechSynthesis.cancel();
 
         splitTextArr.forEach(function (splitText) {
-            const uttr = new SpeechSynthesisUtterance(splitText)
-            console.log(splitText);
+            const uttr = new SpeechSynthesisUtterance(splitText);
 
-            uttr.voice = speechSynthesis
-                .getVoices()
-                .filter((voice) => voice.name === "Google US English")[0]
+            uttr.voice = speechSynthesis.getVoices().filter((voice) => voice.name === "Google US English")[0];
             const rate = parseFloat(rateInput.value);
-            uttr.rate = rate
+            uttr.rate = rate;
 
-            //speechSynthesis.cancel();
             speechSynthesis.speak(uttr);
         })
     }
-})
+});
 
 stopBtn.addEventListener('click', () => {
     let btnText = document.querySelector('#stop-btn').textContent;
 
     if (btnText === '一時停止') {
         speechSynthesis.pause();
-        document.querySelector('#stop-btn').innerHTML = '再開'
+        document.querySelector('#stop-btn').innerHTML = '再開';
     }
     if (btnText === '再開') {
         speechSynthesis.resume();
-        document.querySelector('#stop-btn').innerHTML = '一時停止'
+        document.querySelector('#stop-btn').innerHTML = '一時停止';
     }
-})
+});
--- resources/js/text.js
+++ resources/js/text.js
@@ -4,24 +4,11 @@ document.addEventListener('DOMContentLoaded', function () {
 
     links.forEach(function (link) {
         link.addEventListener('click', function (event) {
-            event.preventDefault(); // デフォルトのリンク動作を無効化
-
             let text = link.querySelector('.text').textContent;
             text = text.trim();
 
             let element = document.querySelector('textarea');
             element.value = text;
-
         });
     });
 });
-
-
-const speakBtn = document.querySelector('#speak-btn')
-const stopBtn = document.querySelector('#stop-btn')
-
-speakBtn.addEventListener('click', () => {
-    let textElement = document.querySelector('#text')
-});
-
-
